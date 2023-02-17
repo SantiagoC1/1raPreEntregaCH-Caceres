dbc6661f7afced3d8902794f29f01343f90b4ba9
SantiagoC1 <1caceres.santiago5@gmail.com>
2023-02-17 10:59:51 -0300

    empezando JS

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/* 1er paso 
+    quieres hacer una compra en KEFLA?
+    1 si 
+    2 no
+   2do paso 
+    KEFLA es una empresa que se dedica a la fabricacion de buzos
+    Que especie de buzo le gustaria comprar?
+    1 estampado
+    2 bordado
+    3 liso
+    Usted eligio un buzo (tipo de buzo elegido), los modelos se mostraran visualmente mas adelante, por el momento le dejaremos los nombre para que elija es que mas le gusta
+
+    1
+    2
+    3
+    4
+    5
+
+    usted eligio (tipo de buzo elegido) que elije?
+    1 talle 1 ancho: largo:
+    2 talle 2 ancho: largo:
+    3 talle 3 ancho: largo:
+    4 talle 4 ancho: largo:
+    5 talle 5 ancho: largo:
+    
+    El importe del buzo (tipo de buzo) es $(precio) como desea abonar?
+    1 efectivo 10%OFF
+    2 tranferecia 
+    3 Mercado Pago 20% de recargo
+    4 cuotas (1-2-3-6-12)
+
+    Muchas gracias por su compra 
+    quieres hacer una compra en KEFLA?
+    1 si 
+    2 no
+    
+     */
