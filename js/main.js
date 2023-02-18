8305dbeebcab78ebad26c1ec87a9de89ffe4d1c2
SantiagoC1 <1caceres.santiago5@gmail.com>
2023-02-17 21:29:53 -0300

    actualizando JS

--- js/main.js
+++ js/main.js
@@ -1,73 +1,94 @@
-/* 1er paso 
-    quieres hacer una compra en KEFLA?
-    1 si 
-    2 no
-   2do paso 
-    KEFLA es una empresa que se dedica a la fabricacion de buzos
-    Que tipo de buzo le gustaria comprar?
-    1 estampado
-    2 bordado
-    3 liso
-    Usted eligio un buzo (tipo de buzo elegido), los modelos se mostraran visualmente mas adelante, por el momento le dejaremos los nombre para que elija es que mas le gusta
-
-    1 Princess
-    2 BYS
-    3 Mood
-    4 Bluewave
-    5 Rose
-
-    usted eligio (tipo de buzo elegido) que talle usas?
-    1 talle 1 ancho: largo:
-    2 talle 2 ancho: largo:
-    3 talle 3 ancho: largo:
-    4 talle 4 ancho: largo:
-    5 talle 5 ancho: largo:
-    
-    El importe del buzo (tipo de buzo) es $(precio) como desea abonar?
-    1 efectivo 10%OFF
-    2 tranferecia 
-    3 Mercado Pago 20% de recargo
-    4 cuotas (1-2-3-6-12)
-
-    Muchas gracias por su compra 
-    quieres hacer una compra en KEFLA?
-    1 si 
-    2 no
-    
-     */
-
+    const estampado=17000,liso=11000,bordado=14000;
+    let talle;
     let respuesta = parseInt(prompt("Quiere realizar una compra?\n 1 si\n 2 no"));
-    let p = "p: Princess $13499 ";
-    let b = "b: BYS $14200 ";
-    let m = "m: Mood $15499 ";
-    let w = "w: Bluewave $15499 ";
-    let r = "r: Rose $13499 ";
+    
+    function talles (tipo){
+        talle = prompt(`Usted eligio un buzo ${tipo} , que talle desea elegir?\n 1 talle 1 ancho:62cm largo:70cm\n 2 talle 2 ancho:68cm largo:74cm\n 3 talle 3 ancho:71cm largo:77cm\n 4 talle 4 ancho:78cm largo:81cm\n 5 talle 5 ancho:85cm largo:84cm `);
+    }
+    function calculo (t,v,c){
+        t= v + (v*0.24);
+                    alert(`El total a pagar es de $${t} abonando en ${c} cuotas`);
+                    parseInt(c);
+                    t=Math.trunc(t/c);
+                    alert(`Abonando un total de $${t} por cada cuota`); 
+    }
 
-    function metodoPago() {};
+    function cuotas(t,v) {
+        let c= prompt("en cuantas cuotas quiere realizar el pago?\n 2 cuotas +24%\n 3 cuotas +43%\n 6 cuotas +76%\n 12 cuotas +82%");
+        switch (c) {
+            case '2':
+                    calculo(t,v,c);               
+                break;
+            case '3':
+                    calculo(t,v,c);              
+                break;
+            case '6': 
+                    calculo(t,v,c); 
+                break;
+            case '12':
+                    calculo(t,v,c);              
+                break;
+            default:
+                break;
+        }
+    }
+
+    function metodoPago(valor) {
+        let total;
+        let forma= prompt("1 efectivo 10%OFF\n2 tranferecia\n 3 Mercado Pago 20% de recargo\n 4 cuotas (2-3-6-12)");
+        switch (forma) {
+            case '1':
+                total= valor - (valor * 0.1);
+                alert(`El valor total que tendra que abonar es de $${total}`);
+                break;
+            case '2':
+                total = valor; 
+                alert(`El valor total que tendra que abonar es de $${total}`);
+                break;
+            case '3':
+                total= valor + (valor * 0.2);
+                alert(`El valor total que tendra que abonar es de $${total}`);
+                break;
+            case '4':
+                cuotas(total,valor);
+                break;
+        
+            default:
+                break;
+        }
+    };
 
 
     function venta(){
     
         let tipo = prompt("KEFLA es una empresa que se dedica a la fabricacion de buzos\n Que tipo de buzo le gustaria comprar?\n 1 Estampado\n 2 Liso\n 3 bordado");
-         
-        switch (tipo) {
+            switch (tipo) {
             case 'estampado':
-                 let model = prompt(`Usted eligio un buzo ${tipo}, los modelos se mostraran visualmente mas adelante, por el momento le dejaremos los nombre para que elija es que mas le gusta\n ${p}\n ${b}\n ${m}\n ${w}\n ${r}`);
+                 talles(tipo);
+                 alert(`El valor de su buzo es de $${estampado} como desea abonarlo?`);
+                 metodoPago(estampado);
+                 tipo=flase;
                 break;
             case 'liso':
-                let modelo = prompt(`Usted eligio un buzo ${tipo}, los modelos se mostraran visualmente mas adelante, por el momento le dejaremos los nombre para que elija es que mas le gusta\n ${p}\n ${b}\n ${m}\n ${w}\n ${r}`);
+                talles(tipo);
+                alert(`El valor de su buzo es de $${liso} como desea abonarlo?`);
+                metodoPago(liso);
+                tipo=flase;
                 break;
             case 'bordado':
-                let mod = prompt(`Usted eligio un buzo ${tipo}, los modelos se mostraran visualmente mas adelante, por el momento le dejaremos los nombre para que elija es que mas le gusta\n ${p}\n ${b}\n ${m}\n ${w}\n ${r}`);
+                talles(tipo);
+                alert(`El valor de su buzo es de $${bordado} como desea abonarlo?`);
+                metodoPago(bordado);
+                tipo=flase;
                 break;
-            default: prompt("El tipo de buzo fue mal ingresado, por favor ingreselo de nuevo");
+            default: alert("El tipo de buzo fue mal ingresado, por favor ingreselo de nuevo");
                      tipo = prompt("KEFLA es una empresa que se dedica a la fabricacion de buzos\n Que tipo de buzo le gustaria comprar?\n 1 Estampado\n 2 Liso\n 3 bordado");
                 break;
-        }
+            }
     }
 
-    if (respuesta === 1){
-        venta();
-    }else{
-        alert("Gracias por visitarnos")
-    }
+if (respuesta === 1){
+       venta();
+   }else{
+       alert("Gracias por visitarnos")
+   }
