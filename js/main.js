10085ca5c46ed3652e93a76898cd84393102a8b6
SantiagoC1 <1caceres.santiago5@gmail.com>
2023-03-02 20:36:41 -0300

    actualizando js

--- js/main.js
+++ js/main.js
@@ -1,92 +1,91 @@
-    const estampado=17000,liso=11000,bordado=14000;
-    let respuesta = parseInt(prompt("Quiere realizar una compra?\n 1 si\n 2 no"));
+const clave = "kefla2023";
+let cantCli = 0, totalVendido = 0, i = 0,totalVenta=0;
+    const pagos = [
+        {metodo:"efectivo",funcion:0.1},
+        {metodo:"transferencia",funcion:0},
+        {metodo:"mercado pago",funcion:0.2},
+        {metodo:"2 cuotas",funcion:0.24},
+        {metodo:"3 cuotas",funcion:0.43},
+        {metodo:"6 cuotas",funcion:0.76},
+        {metodo:"12 cuotas",funcion:0.82},
+    ];
+    const buzo = [
+        {tipo:"estampado",nombre:"bys", precio:14200,stock:50},
+        {tipo:"estampado",nombre:"moodg", precio:15499,stock:50},
+        {tipo:"estampado",nombre:"princess", precio:13499,stock:50},
+        {tipo:"estampado",nombre:"bluewave", precio:15499,stock:50},
+        {tipo:"estampado",nombre:"clow", precio:14499,stock:50},
+        {tipo:"estampado",nombre:"happycake", precio:15000,stock:50},
+        {tipo:"estampado",nombre:"bekind", precio:17000,stock:50},
+        {tipo:"estampado",nombre:"tyv", precio:15000,stock:50},
     
-    // Funcion que calcula el total precio total del buzo
-    function calculo (t,v,c){
-        t= v + (v*0.24);
-                    alert(`El total a pagar es de $${t} abonando en ${c} cuotas`);
-                    parseInt(c);
-                    t=Math.trunc(t/c);
-                    alert(`Abonando un total de $${t} por cada cuota`); 
-    }
-    // funcion que pregunta la cantidad de cuotas en las que se quiere abonar
-    function cuotas(t,v) {
-        let c= prompt("en cuantas cuotas quiere realizar el pago?\n 2 cuotas +24%\n 3 cuotas +43%\n 6 cuotas +76%\n 12 cuotas +82%");
-        switch (c) {
-            case '2':
-                    calculo(t,v,c);               
-                break;
-            case '3':
-                    calculo(t,v,c);              
-                break;
-            case '6': 
-                    calculo(t,v,c); 
-                break;
-            case '12':
-                    calculo(t,v,c);              
-                break;
-            default:
-                break;
-        }
-    }
-    // Funcion que calcula el metodo de pago
-    function metodoPago(valor) {
-        let total;
-        let forma= prompt("1 efectivo 10%OFF\n2 tranferecia\n 3 Mercado Pago 20% de recargo\n 4 cuotas (2-3-6-12)");
-        switch (forma) {
-            case '1':
-                total= valor - (valor * 0.1);
-                alert(`El valor total que tendra que abonar es de $${total}`);
-                break;
-            case '2':
-                total = valor; 
-                alert(`El valor total que tendra que abonar es de $${total}`);
-                break;
-            case '3':
-                total= valor + (valor * 0.2);
-                alert(`El valor total que tendra que abonar es de $${total}`);
-                break;
-            case '4':
-                cuotas(total,valor);
-                break;
-        
-              default:
-                break;
-        }
-    };
-
-    //funcion que realiza la venta del producto
-    function venta(){
+        {tipo:"liso",nombre:"verde",precio:11000,stock:50},
+        {tipo:"liso",nombre:"rojo",precio:11000,stock:50},
+        {tipo:"liso",nombre:"marino",precio:11000,stock:50},
+        {tipo:"liso",nombre:"azul",precio:11000,stock:50},
+        {tipo:"liso",nombre:"celeste",precio:11000,stock:50},
+        {tipo:"liso",nombre:"violeta",precio:11000,stock:50},
+        {tipo:"liso",nombre:"cremita",precio:11000,stock:50},
+        {tipo:"liso",nombre:"verdeagua",precio:11000,stock:50},
     
-        let tipo = prompt("KEFLA es una empresa que se dedica a la fabricacion de buzos\n Que tipo de buzo le gustaria comprar?\n -Estampado\n -Liso\n -bordado");
-            switch (tipo.toLowerCase()) {
-            case 'estampado':
-                talle = prompt(`Usted eligio un buzo ${tipo} , que talle desea elegir?\n 1 talle 1 ancho:62cm largo:70cm\n 2 talle 2 ancho:68cm largo:74cm\n 3 talle 3 ancho:71cm largo:77cm\n 4 talle 4 ancho:78cm largo:81cm\n 5 talle 5 ancho:85cm largo:84cm `);
-                 alert(`El valor de su buzo es de $${estampado} como desea abonarlo?`);
-                 metodoPago(estampado);
-                 tipo=flase;
-                break;
-            case 'liso':
-                talle = prompt(`Usted eligio un buzo ${tipo} , que talle desea elegir?\n 1 talle 1 ancho:62cm largo:70cm\n 2 talle 2 ancho:68cm largo:74cm\n 3 talle 3 ancho:71cm largo:77cm\n 4 talle 4 ancho:78cm largo:81cm\n 5 talle 5 ancho:85cm largo:84cm `);
-                alert(`El valor de su buzo es de $${liso} como desea abonarlo?`);
-                metodoPago(liso);
-                tipo=flase;
-                break;
-            case 'bordado':
-                talle = prompt(`Usted eligio un buzo ${tipo} , que talle desea elegir?\n 1 talle 1 ancho:62cm largo:70cm\n 2 talle 2 ancho:68cm largo:74cm\n 3 talle 3 ancho:71cm largo:77cm\n 4 talle 4 ancho:78cm largo:81cm\n 5 talle 5 ancho:85cm largo:84cm `);
-                alert(`El valor de su buzo es de $${bordado} como desea abonarlo?`);
-                metodoPago(bordado);
-                tipo=flase;
-                break;
-            default: alert("El tipo de buzo fue mal ingresado, por favor ingreselo de nuevo");
-                     tipo = prompt("KEFLA es una empresa que se dedica a la fabricacion de buzos\n Que tipo de buzo le gustaria comprar?\n 1 Estampado\n 2 Liso\n 3 bordado");
+        {tipo:"bordado",nombre:"rosa", precio:14500,stock:50},
+        {tipo:"bordado",nombre:"trueno", precio:14500,stock:50},
+        {tipo:"bordado",nombre:"pez", precio:14500,stock:50},
+        {tipo:"bordado",nombre:"nube", precio:14500,stock:50},
+        {tipo:"bordado",nombre:"mariposa", precio:14500,stock:50},
+        {tipo:"bordado",nombre:"corona", precio:14500,stock:50},];
+    const carrito = [];
 
-            }
-    }
+//Programa pricipal    
+let nombre = prompt("Ingrese su nombre");
 
-    if (respuesta === 1){
-        venta();
+while (nombre !== "zzz") {
+    if (nombre === "admin") {
+        let contrasenia = prompt("Ingrese su contraseña");
+        if (contrasenia === clave) {
+            alert("Usted accedio a la parte de administrador");
+            //hacer acciones que se puedan modificar los objetos
+        }
     }else {
-        alert("Gracias por visitarnos");
+        alert(`Bienvenido ${nombre} a KEFLA.\n  Somos una empresa que se dedica a la fabricacion de buzos`);
+        let compra = prompt("Quiere realizar una compra?\n-Si \n-No");
+        while (compra === "si"){
+            let tipo = prompt("que tipo de buzo le gustaria comprar\n-Estampado\n-Liso\n-Bordado");
+            mostrar(tipo.toLowerCase());
+            let modelo = prompt("Habiendo visto nuestros modelos, Cual le gustaria comprar?");
+            let item = buzo.find(item => item.nombre === modelo);
+            carrito.push(item);
+            i=buzo.indexOf(item);
+            buzo[i].stock=buzo[i].stock -1;
+            let msj="";
+            for (const prod of carrito){
+                let header =`${nombre} este es tu CARRITO:\n`;
+                msj += ` Nombre:${prod.nombre}\n Precio:$${prod.precio}\n`;
+                alert(header+msj);
+            }
+            for (const producto of carrito){
+                totalVenta=totalVenta+ producto.precio;
+            }
+            alert(`$${totalVenta}`);
+            compra = prompt("Quiere realizar otra compra?\n-Si \n-No");
+        }
+        //obtener el valor total de lo comprado por el cliente 
+        //funcion de metodo de pago pasandole el parametro del total de la compra
+        alert(`Gracias por visitarnos ${nombre}`);
     }
-    
+    //cantCli++; sumo el cliente
+    //totalVendido=totalVendido+total //sumo la cantidad total de lo vendido entre todas las ventas
+    nombre = prompt("Ingrese su nombre");//
+}
+//Funcion que muestra los productos del tipo seleccionado
+function mostrar (tipo){
+    let encontre = buzo.filter(item => item.tipo === tipo);
+    encontre.forEach(item =>{
+        let mensaje = `BUZOS ${tipo}s:\n
+        Nombre: ${item.nombre}
+        Precio: $${item.precio}
+        Stock: ${item.stock}
+        `;
+        alert(mensaje);
+    });
+}
