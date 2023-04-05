14e0f81baf0147743e033fd2918773ad59a421ff
SantiagoC1 <1caceres.santiago5@gmail.com>
2023-04-05 15:29:52 -0300

    modificando rutas de acceso

--- js/stock.js
+++ js/stock.js
@@ -1,6 +1,6 @@
 const traerProd = async() =>{
     try{
-        const response = await fetch(".../stock.json");
+        const response = await fetch("../../stock.json");
         const stock = await response.json();
         return stock;
     }catch(error){
